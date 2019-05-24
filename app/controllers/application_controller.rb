a336a161f12f1f971fa3c40d25d3c6b184611e0c
musa <lucasvmachado@gmail.com>
2019-05-23 22:46:37 -0300

    Fix pundit missing configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Response
   include ExceptionHandler
+  include Pundit
+
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
 end
