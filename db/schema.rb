e7ffe9fff4a1d26008c0232fa08afd9b90655a56
musa <lucasvmachado@gmail.com>
2019-05-23 22:06:08 -0300

    Add pgcryto extension to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_000833) do
+ActiveRecord::Schema.define(version: 2019_05_24_010420) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
